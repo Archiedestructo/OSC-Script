bbdbe6208e00fa6daf98f921a5d0443434e661d5
Drew Schmidt <drew.schmidt@nwciowa.edu>
2014-04-30 10:35:27 -0500

    Added more verbose try / catch

--- dist/OSC Script.app/Contents/Resources/OSC Script.py
+++ dist/OSC Script.app/Contents/Resources/OSC Script.py
@@ -17,10 +17,16 @@ except ImportError:
     os._exit(0) 
 
 def OSCResponse(addr, source, message):
-    clnt = OSCClient()
-    clientAddress = ( source[0], 53000 )
-    clnt.connect(clientAddress)
-    clnt.send( OSCMessage(addr + " " + message ) )
+    try:
+        print addr
+        print source
+        print message
+        clnt = OSCClient()
+        clientAddress = ( source[0], 53000 )
+        clnt.connect(clientAddress)
+        clnt.send( OSCMessage(addr + " " + message ) )
+    except Exception, e:
+        print str(e)
 
 def ScriptPath_Handler(addr, tags, data, source):
     print data[0]
@@ -28,8 +34,9 @@ def ScriptPath_Handler(addr, tags, data, source):
         print "osascript " + data[0].replace(" ", "\\ ") + ""
         os.system("osascript " + data[0].replace(" ", "\\ ") + "")
         OSCResponse(addr + "/Sent", source, str(data[0].replace(" ", "\\ ")))
-    except:
-        OSCResponse(addr + "/Error", source, sys.exc_info()[0])
+    except Exception, e:
+        print str(e)
+        OSCResponse(addr + "/Error", source, str(e))
 
 def ScriptCode_Handler(addr, tags, data, source):
     print data[0]
@@ -39,8 +46,9 @@ def ScriptCode_Handler(addr, tags, data, source):
         p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
         stdout, stderr = p.communicate(message)
         OSCResponse(addr + "/Sent", source, str(message))
-    except:
-        OSCResponse(addr + "/Error", source, sys.exc_info()[0])
+    except Exception, e:
+        print str(e)
+        OSCResponse(addr + "/Error", source, str(e))
     
 def UDP_Handler(addr, tags, data, source):
     if (len(data) == 3):
@@ -52,49 +60,95 @@ def UDP_Handler(addr, tags, data, source):
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             sock.sendto(data[2].encode(encoding='UTF-8'), (str(data[0]), int(data[1])))
             OSCResponse(addr + "/Sent", source, str(data[2]))
-        except:
-            OSCResponse(addr + "/Error", source, sys.exc_info()[0])
+        except Exception, e:
+            print str(e)
+            OSCResponse(addr + "/Error", source, str(e))
 
 def Telnet_Handler(addr, tags, data, source):
+    global TelnetUsername
+    global TelnetPassword
+    global TelnetTimeout
+    global TelnetDelay
     OSCResponse(addr, source, "Attempting to send Telnet Command: " + data[1])
     if (data[0].count(" ") > 0):
         try:
-            tn = telnetlib.Telnet(data[0].split(" ")[0], int(data[0].split(" ")[1]), 30)
+            tn = telnetlib.Telnet(data[0].split(" ")[0], int(data[0].split(" ")[1]), TelnetTimeout)
+            if (TelnetUsername != "" and TelnetPassword != ""):
+                tn.read_until("Login:", TelnetTimeout)
+                tn.write(TelnetUsername + "\n")
+                tn.read_until("Password:", TelnetTimeout)
+                tn.write(TelnetPassword + "\n")
             tn.write(str(data[1]) + "\r")
-            time.sleep(.1)
+            time.sleep(TelnetDelay)
             tn.close()
             OSCResponse(addr + "/Sent", source, str(data[1]))
-        except:
-           OSCResponse(addr + "/Error", source, sys.exc_info()[0])
+        except Exception, e:
+            print str(e)
+            OSCResponse(addr + "/Error", source, str(e))
     else:
         try:
-            tn = telnetlib.Telnet(data[0], 23, 30)
+            tn = telnetlib.Telnet(data[0], 23, TelnetTimeout)
             tn.write(str(data[1]) + "\r")
-            time.sleep(.1)
+            if (TelnetUsername != "" and TelnetPassword != ""):
+                tn.read_until("Login:", TelnetTimeout)
+                tn.write(TelnetUsername + "\n")
+                tn.read_until("Password:", TelnetTimeout)
+                tn.write(TelnetPassword + "\n")
+            time.sleep(TelnetDelay)
             tn.close()
             OSCResponse(addr + "/Sent", source, str(data[1]))
-        except:
-           OSCResponse(addr + "/Error", source, sys.exc_info()[0])
+        except Exception, e:
+            print str(e)
+            OSCResponse(addr + "/Error", source, str(e))
 
 TelnetUsername = ""                   
 def TelnetUsername_Handler(addr, tags, data, source):
-    OSCResponse(addr, source, "Set Telnet Username: " + data[0])
-    global TelnetUsername
-    TelnetUsername = data[0]
+    try:
+        OSCResponse(addr, source, "Set Telnet Username: " + data[0])
+        global TelnetUsername
+        TelnetUsername = data[0]
+    except Exception, e:
+        print str(e)
+        OSCResponse(addr + "/Error", source, str(e))
 
 TelnetPassword = ""                   
 def TelnetPassword_Handler(addr, tags, data, source):
-    OSCResponse(addr, source, "Set Telnet Password: " + data[0])
-    global TelnetPassword
-    TelnetPassword = data[0]
+    try:
+        OSCResponse(addr, source, "Set Telnet Password: " + data[0])
+        global TelnetPassword
+        TelnetPassword = data[0]
+    except Exception, e:
+        print str(e)
+        OSCResponse(addr + "/Error", source, str(e))
+    
+TelnetTimeout = 30                   
+def TelnetTimeout_Handler(addr, tags, data, source):
+    try:
+        OSCResponse(addr, source, "Set Telnet Timeout: " + str(data[0]))
+        global TelnetTimeout
+        TelnetTimeout = float(data[0])
+    except Exception, e:
+        print str(e)
+        OSCResponse(addr + "/Error", source, str(e))
+    
+TelnetDelay = 0.1                   
+def TelnetDelay_Handler(addr, tags, data, source):
+    try:
+        OSCResponse(addr, source, "Set Telnet Delay: " + str(data[0]))
+        global TelnetDelay
+        TelnetDelay = float(data[0])
+    except Exception, e:
+        print str(e)
+        OSCResponse(addr + "/Error", source, str(e))
                     
 def Terminal_Handler(addr, tags, data, source):
     print data[0]
     try:
         os.system(data[0].replace("'", "\""))
         OSCResponse(addr + "/Sent", source, str(data[0]))
-    except:
-        OSCResponse(addr + "/Error", source, sys.exc_info()[0])
+    except Exception, e:
+        print str(e)
+        OSCResponse(addr + "/Error", source, str(e))
         
 def Exit_Handler(addr, tags, data, source):
     print "\nClosing OSCServer."
@@ -114,6 +168,8 @@ try:
     srvr.addMsgHandler("/runScript/Telnet", Telnet_Handler)
     srvr.addMsgHandler("/runScript/Telnet/SetUsername", TelnetUsername_Handler)
     srvr.addMsgHandler("/runScript/Telnet/SetPassword", TelnetPassword_Handler)
+    srvr.addMsgHandler("/runScript/Telnet/SetTimeout", TelnetTimeout_Handler)
+    srvr.addMsgHandler("/runScript/Telnet/SetDelay", TelnetDelay_Handler)
     srvr.addMsgHandler("/runScript/Terminal", Terminal_Handler)
     srvr.addMsgHandler("/quit", Exit_Handler)
     srvr.addMsgHandler("/exit", Exit_Handler)
